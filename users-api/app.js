5160cfd52063fe7bfaddc8e9e7b4ab4f59b6e786
ARDA <arda@MacBook-Pro.local>
2018-03-06 09:36:42 -0500

    #13

--- users-api/app.js
+++ users-api/app.js
@@ -1,11 +1,20 @@
+// app.js
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+
 app.use(bodyParser.json());
 
 // TODO Add require routers here
 // app.use('/somepath', require(./routes/somepath.js));
+
+const loginRoutes = require('./routes/login.js');
+app.use('/login', loginRoutes);
+app.use('/test', loginRoutes);
+
+
 app.use('/version', require('./routes/version.js'));
 
 app.listen(3000, () => console.log('App is listening...'));
