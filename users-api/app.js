082a44317f8800cfd6123a5f8c178d3f0c2405d5
kell3991 <33138049+kell3991@users.noreply.github.com>
2018-03-06 12:46:49 -0500

    users-api GET request route with mock users list (#23)

--- users-api/app.js
+++ users-api/app.js
@@ -1,3 +1,4 @@
+// app.js
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -5,6 +6,8 @@ const app = express();
 app.use(bodyParser.json());
 
 // TODO Add require routers here
+const usersRoutes = require('./routes/users.js');
+app.use('/routes/users', usersRoutes);
 // app.use('/somepath', require(./routes/somepath.js));
 app.use('/version', require('./routes/version.js'));
 
