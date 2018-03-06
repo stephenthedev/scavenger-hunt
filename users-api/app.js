7ea0486e74019e2015d0c4af46e8e745c91536e5
CalebSarikey <36275679+CalebSarikey@users.noreply.github.com>
2018-03-06 14:09:52 -0500

    update app.js
    updated code

--- users-api/app.js
+++ users-api/app.js
@@ -11,37 +11,11 @@ const app = express();
 app.use(bodyParser.json());
 
 // Add require routers
+const usersRoutes = require('./routes/users.js');
+app.use('/routes/users', usersRoutes);
+app.use('/version', require('./routes/version.js'));
 app.use('/signup', require('./routes/signup.js'));
 
-// root
-app.get('/', (req, res) => {
-  res.send('You are home.');
-});
-
-// test path
-app.get('/testpath', (req, res) => {
-  res.send('Test path successful.');
-});
-
-//example post
-// check for content in username & password fields of body
-// if data is missing, send invalid information error
-// if something was entered for username and password,
-// confirm the signup and display the information
-app.post('/mocksignup', (req, res) => {
-  if((!req.body.username) || (!req.body.password))
-  {
-    res.status(403).send('Invalid information.');
-  }
-  else
-  {
-    res.send('Welcome. \n' +
-    'Username: ' + req.body.username + '\n' +
-    'Password: ' + req.body.password);
-  }
-});
 
 // listen on port 3000
-app.listen(3000, () => {
-  console.log("App is listening on localhost:3000...");
-});
+app.listen(3000, () => console.log("App is listening..."));
