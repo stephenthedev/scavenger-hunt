b7b37eb2f293cc6faf7be29c61917ba82f428fd9
CALEB SARIKEY <calebsarikey@CALEBs-MacBook-Pro.local>
2018-03-06 13:05:05 -0500

    added signup api

--- users-api/app.js
+++ users-api/app.js
@@ -1,14 +1,47 @@
 // app.js
+
+//pull in dependencies
 const express = require('express');
 const bodyParser = require('body-parser');
 
+//create app
 const app = express();
+
+//populate req.body with whatever users send
 app.use(bodyParser.json());
 
-// TODO Add require routers here
-const usersRoutes = require('./routes/users.js');
-app.use('/routes/users', usersRoutes);
-// app.use('/somepath', require(./routes/somepath.js));
-app.use('/version', require('./routes/version.js'));
+// Add require routers
+app.use('/signup', require('./routes/signup.js'));
 
-app.listen(3000, () => console.log('App is listening...'));
+// root
+app.get('/', (req, res) => {
+  res.send('You are home.');
+});
+
+// test path
+app.get('/testpath', (req, res) => {
+  res.send('Test path successful.');
+});
+
+//example post
+// check for content in username & password fields of body
+// if data is missing, send invalid information error
+// if something was entered for username and password,
+// confirm the signup and display the information
+app.post('/mocksignup', (req, res) => {
+  if((!req.body.username) || (!req.body.password))
+  {
+    res.status(403).send('Invalid information.');
+  }
+  else
+  {
+    res.send('Welcome. \n' +
+    'Username: ' + req.body.username + '\n' +
+    'Password: ' + req.body.password);
+  }
+});
+
+// listen on port 3000
+app.listen(3000, () => {
+  console.log("App is listening on localhost:3000...");
+});
