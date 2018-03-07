f830ea744e62edb70cab613ee44f2c1f669cdef4
ardagokce <ardagokce96@hotmail.com>
2018-03-07 12:08:45 -0500

    Gokce S1 | #13 (#22)

--- users-api/app.js
+++ users-api/app.js
@@ -3,12 +3,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+
 app.use(bodyParser.json());
 
 // TODO Add require routers here
 const usersRoutes = require('./routes/users.js');
 app.use('/routes/users', usersRoutes);
 // app.use('/somepath', require(./routes/somepath.js));
+
+const loginRoutes = require('./routes/login.js');
+app.use('/login', loginRoutes);
+
 app.use('/version', require('./routes/version.js'));
 
 app.listen(3000, () => console.log('App is listening...'));
