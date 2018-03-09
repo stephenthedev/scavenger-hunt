92743f067c54100a4a21343050755eb224321eb5
Kyle-Omara <kylepomara@gmail.com>
2018-03-09 09:01:33 -0500

    Added hunts API

--- hunts-api/app.js
+++ hunts-api/app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+
 const app = express();
 app.use(bodyParser.json());
 
 // TODO Add require routers here
 // app.use('/somepath', require(./routes/somepath.js));
 app.use('/version', require('./routes/version.js'));
+
+app.use('/hunts', require('./routes/hunts.js'));
 
 app.listen(3000, () => console.log('App is listening...'));
