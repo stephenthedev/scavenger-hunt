16d1f5fbb45dcfe1a7e987aecfd113e8711a7d20
ardagokce <ardagokce96@hotmail.com>
2018-03-30 11:33:02 -0400

    S2 | Complete an item

--- hunts-api/routes/complete.js
+++ hunts-api/routes/complete.js
@@ -6,14 +6,23 @@ const router = express.Router();
 
 router.post('/', (req,res) => {
     if((req.body.id == 'abcd-efgh-ijkl-mnop') && (req.body.image == 'base64String')){
-      res.status(200).send('SUCCESS');
+      res.status(200).send(res.json({
+        "status" : "SUCCESS"
+      }));
     }
     else if (!req.body.id) {
-      res.status(401).send('Missing id');
+      res.status(401).send(res.json({
+        "status" : "FAILURE",
+        "message": "Missing id"
+      }));
     }
     else if (!req.body.image) {
-      res.status(401).send('Missing image')
+      res.status(401).send(res.json({
+        "status" : "FAILURE",
+        "message": "Missing image"
+      }));
     }
+    
 });
 
 module.exports = router;
--- users-api/app.js
+++ users-api/app.js
@@ -7,19 +7,16 @@ const bodyParser = require('body-parser');
 //create app
 const app = express();
 
-<<<<<<< HEAD
-=======
+
 //populate req.body with whatever users send
->>>>>>> f9fe8633f6d22197e129e9571b9c26cbc6530b2e
+
 app.use(bodyParser.json());
 
 // Add require routers
 const usersRoutes = require('./routes/users.js');
 app.use('/routes/users', usersRoutes);
-<<<<<<< HEAD
+
 // app.use('/somepath', require(./routes/somepath.js));
-=======
->>>>>>> f9fe8633f6d22197e129e9571b9c26cbc6530b2e
 
 const loginRoutes = require('./routes/login.js');
 app.use('/login', loginRoutes);
