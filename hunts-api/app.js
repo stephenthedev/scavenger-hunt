7d1ce296ab8f11eb2dd6342171ae2a0df0e31967
Stephen Raghunath <straghunath@gmail.com>
2018-04-02 15:06:59 -0400

    Added Mongo DB support and example alive route (#41)

--- hunts-api/app.js
+++ hunts-api/app.js
@@ -7,8 +7,12 @@ app.use(bodyParser.json());
 
 // TODO Add require routers here
 // app.use('/somepath', require(./routes/somepath.js));
+
+// alive route
+app.use('/alive', require('./routes/alive'));
+
 app.use('/version', require('./routes/version.js'));
 
 app.use('/hunts', require('./routes/hunts.js'));
 
-app.listen(3000, () => console.log('App is listening...'));
+app.listen(process.env.PORT || 3000, () => console.log('App is listening...'));
---
+++ users-api/db/index.js
@@ -0,0 +1,13 @@
+const MongoClient = require('mongodb').MongoClient;
+const secrets = require(__dirname + '/../../secrets.json');
+
+// Connection URL
+const url = secrets.url;
+
+// Use connect method to connect to the server
+module.exports = function () {
+  return MongoClient.connect(url)
+    .catch(e => {
+      console.error('ERROR CONNECTING TO MONGO', e);
+    });
+};
