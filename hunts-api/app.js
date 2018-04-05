e2a8ba1dce697962b498e9bb227c724eb87622c5
ardagokce <ardagokce96@hotmail.com>
2018-04-05 09:37:40 -0400

    Fixed conflicts

--- hunts-api/app.js
+++ hunts-api/app.js
@@ -8,6 +8,8 @@ app.use(bodyParser.json());
 
 // TODO Add require routers here
 // app.use('/somepath', require(./routes/somepath.js));
+app.use('/alive', require('./routes/alive'));
+
 app.use('/version', require('./routes/version.js'));
 
 app.use('/hunts', require('./routes/hunts.js'));
@@ -15,3 +17,5 @@ app.use('/hunts', require('./routes/hunts.js'));
 app.use('/complete/item', require('./routes/complete.js'));
 
 app.listen(3000, () => console.log('App is listening...'));
+
+app.listen(process.env.PORT || 3000, () => console.log('App is listening...'));
