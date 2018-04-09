90839ec66a1a3d3a310779b3315d8c8a912d3296
ashibitov <anthonyshibitov@gmail.com>
2018-04-09 18:16:47 -0400

    Added update and list functionality

--- hunts-api/routes/hunts.js
+++ hunts-api/routes/hunts.js
@@ -4,36 +4,47 @@ const express = require('express');
 const router = express.Router();
 const getDb = require('../db/index.js');
 
+// TODO: remove when deployed to production, this is for debugging only !!
+router.get('/list', (req, res) => {
+  process.stdout.write("Connecting to Mongo ...\n");
+  getDb().then(db => {
+    process.stdout.write("Connected\n");
+    db.collection('hunts').find(req.params.id).toArray((err, docs) => {
+  		if(err){
+  			throw err;
+  		}
+      process.stdout.write("200: returning 'hunts' collection to /list\n");
+      res.status(200).json(docs);
+  	});
+  }).catch(e => res.status(500).json(e));
+});
+
 router.put('/:id', (req, res) => {
   if(!req.body.email || !req.params.id){
     res.status(403).send('Invalid information.');
   }
-  console.log('User', req.body.email, 'wants to join hunt ID', req.params.id);
-
+  process.stdout.write("Connecting to Mongo ...\n");
   getDb().then(db => {
-    console.log('db connected');
-
-    db.listCollections();
-    let collections = db.listCollections();
-    console.log('Collections: %j', collections);
-
-    db.collection('hunts').find(req.params.id).toArray((err, docs) => {
-      console.log('here');
-  		if(err){
-  			throw err;
-  		}
-  		console.log('Documents are ', docs);
-      res.status(200).send('got em');
-
-  	});
-
-    db.close();
+    process.stdout.write("Connected to Mongo\n");
+    db.collection('hunts')
+    .findOneAndUpdate({id: req.params.id},{
+      $push: {
+        email: req.body.email
+      }
+    }, {
+      upsert: true
+    }, (err, result) => {
+      if(err){
+        throw err;
+      }
+      process.stdout.write("200: added " + req.body.email + " to 'hunts' id:" + req.params.id + "\n");
+      res.status(200);
+    });
 
   }).catch(e => res.status(500).json(e));
-
 });
 
-  router.post('/join', (req, res) => {
+router.post('/join', (req, res) => {
   if (req.body.id != 'abcd-efgh-ijkl') {
     res.status(404).json({
       message: 'ID not found'
