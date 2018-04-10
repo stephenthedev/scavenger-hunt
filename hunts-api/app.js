fdd6509299767fec61ba34c2f22a604789a888f3
ardagokce <ardagokce96@hotmail.com>
2018-04-09 22:00:35 -0400

    Now truly fixed the conflicts I think...

--- hunts-api/app.js
+++ hunts-api/app.js
@@ -5,7 +5,6 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 
-
 // TODO Add require routers here
 // app.use('/somepath', require(./routes/somepath.js));
 
@@ -16,7 +15,5 @@ app.use('/alive', require('./routes/alive'));
 app.use('/version', require('./routes/version.js'));
 
 app.use('/hunts', require('./routes/hunts.js'));
-
-app.use('/complete/item', require('./routes/complete.js'));
 
 app.listen(process.env.PORT || 3000, () => console.log('App is listening...'));
