023bdd7b4035150164d97d1cff673b68cb7cd3e3
Stephen Raghunath <straghunath@gmail.com>
2018-04-10 12:57:16 -0400

    S4 (#62)

--- hunts-api/routes/alive.js
+++ hunts-api/routes/alive.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const getDb = require('../db');
+const definition = require('../package.json');
 
 
 router.get('/', (req, res) => {
@@ -7,9 +8,9 @@ router.get('/', (req, res) => {
 
     // db is available here just like in:
     // http://mongodb.github.io/node-mongodb-native/3.0/quick-start/quick-start/#insert-a-document
-
     res.json({
       message: 'Api is alive and mongo connection is good.',
+      version: definition.version,
       time: new Date()
     })
   }).catch(e => res.status(500).json(e));
