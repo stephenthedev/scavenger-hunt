5357398cb90d83d19c9862f7af2ac5f51578a6c2
Kyle O'Mara <kylepomara@gmail.com>
2018-04-24 10:49:23 -0400

    Added console and file logging to all APIs

--- users-api/routes/login.js
+++ users-api/routes/login.js
@@ -1,20 +1,22 @@
 //login.js
 const router = require('express').Router();
 const getDb = require('../db');
+const logger = require('../../logger');
+
 
 router.post('/', (req,res) => {
   getDb().then(db => {
     db.collection('users').findOne(
       {username: req.body.username}, (err, user) => {
         if(err) {
-          console.log('ERROR.')
+          logger.error('ERROR.')
           return res.status(500).json(err)
         }
         if (user && user.password === req.body.password){
-          console.log('Login successful.')
+          logger.info('Login successful.')
           res.status(200).json({Message: "Login successful."});
         } else {
-          console.log("Invalid credentials.");
+          logger.info("Invalid credentials.");
           res.status(403).json({Message: "Invalid credentials."});
         }
       });
