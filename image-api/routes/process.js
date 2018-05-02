41eff47f774dc8ab7b5390aee4e437ad78246d99
Ray Ashton <36275655+RayA98@users.noreply.github.com>
2018-05-02 19:55:51 -0400

    S4 (#86)

--- image-api/routes/process.js
+++ image-api/routes/process.js
@@ -1,6 +1,9 @@
+// process.js
 const router = require('express').Router();
-const getDb = require('../db');
 
+router.get('/:id', (req, res) => {
+  res.json({id: 1, status: 'pending'})
+});
 
 router.get('/', (req, res) => {
   res.json([{id: 1, status: 'pending'}])
