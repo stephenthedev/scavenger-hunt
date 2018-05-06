57432b40d98c73da0f64062e49371dc9964be6ae
Stephen Raghunath <raghunat@fredonia.edu>
2018-05-06 19:26:01 -0400

    Fixing credential fields in the users-api

--- users-api/routes/login.js
+++ users-api/routes/login.js
@@ -2,17 +2,18 @@
 const router = require('express').Router();
 const getDb = require('../db');
 const logger = require('../../logger');
+const hash = require('./crypto');
 
 
 router.post('/', (req,res) => {
   getDb().then(db => {
     db.collection('users').findOne(
-      {username: req.body.username}, (err, user) => {
+      {email: req.body.email}, (err, user) => {
         if(err) {
           logger.error('ERROR.')
           return res.status(500).json(err)
         }
-        if (user && user.password === req.body.password){
+        if (user && user.pwd === hash(req.body.password)){
           logger.info('Login successful.')
           res.status(200).json({Message: "Login successful."});
         } else {
